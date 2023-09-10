d2fed9f23076f4c4554bb834cd6bcaa03f36168c
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2023-09-10 18:30:19 +0000

    https server, ssl configuration

--- accelerometer-joystick/move-server.py
+++ accelerometer-joystick/move-server.py
@@ -3,6 +3,12 @@
 import asyncio
 from websockets.server import serve
 import json
+import ssl
+import pathlib
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("cert.pem")
+ssl_context.load_cert_chain(localhost_pem)
 
 async def echo(websocket):
     async for message in websocket:
@@ -26,7 +32,7 @@ async def echo(websocket):
                 print("unsupported")
 
 async def main():
-    async with serve(echo, "0.0.0.0", 8765):
-        await asyncio.Future()  # run forever
+    async with serve(echo, "192.168.0.213", 8765, ssl=ssl_context):
+        await asyncio.Future()
 
 asyncio.run(main())
